c54dfe795a682a96d1df971255a151fdb9ee6fb0
Ben Fiola <benfiola@gmail.com>
2024-11-04 13:08:26 -0800

    Bump minio-operator-ext to 0.1.0-rc.9

--- apps/access-operator.ts
+++ apps/access-operator.ts
@@ -16,12 +16,12 @@ const chartData = {
   crds: {
     chart: "crds",
     repo: "https://benfiola.github.io/access-operator/charts",
-    version: "0.1.0-rc.7",
+    version: "0.1.0-rc.9",
   },
   operator: {
     chart: "operator",
     repo: "https://benfiola.github.io/access-operator/charts",
-    version: "0.1.0-rc.8",
+    version: "0.1.0-rc.9",
   },
 };
 
