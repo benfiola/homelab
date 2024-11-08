66f9fd05e798ca480b019e3255869ada4a3347de
Ben Fiola <benfiola@gmail.com>
2024-11-08 13:08:53 -0800

    add debug logging minecraft

--- apps/minecraft.ts
+++ apps/minecraft.ts
@@ -53,6 +53,7 @@ const manifests: ManifestsCallback = async (app) => {
           EULA: "TRUE",
           MAX_MEMORY: "6G",
           USE_AIKAR_FLAGS: "true",
+          JVM_DD_OPTS: "java.util.logging.ConsoleHandler.level=ALL",
         },
         mounts: {
           data: "/data",
