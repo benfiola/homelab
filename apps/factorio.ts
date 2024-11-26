7ec027ede548780c5d0909a3789e2b7c60ded302
Ben Fiola <benfiola@gmail.com>
2024-11-25 16:41:15 -0800

    update factorio to 2.0.21

--- apps/factorio.ts
+++ apps/factorio.ts
@@ -130,7 +130,7 @@ const manifests: ManifestsCallback = async (app) => {
     containers: [
       {
         envFrom: [serverSecret],
-        image: "factoriotools/factorio:2.0.20",
+        image: "factoriotools/factorio:2.0.21",
         mounts: {
           data: "/factorio",
         },
