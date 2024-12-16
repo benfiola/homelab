30480e88484584b64cfb2944c575a0fa12a9a892
Ben Fiola <benfiola@gmail.com>
2024-12-16 11:47:52 -0800

    update access-operator to 0.1.2

--- apps/access-operator.ts
+++ apps/access-operator.ts
@@ -18,12 +18,12 @@ const chartData = {
   crds: {
     chart: "crds",
     repo: "https://benfiola.github.io/access-operator/charts",
-    version: "0.1.1",
+    version: "0.1.2",
   },
   operator: {
     chart: "operator",
     repo: "https://benfiola.github.io/access-operator/charts",
-    version: "0.1.1",
+    version: "0.1.2",
   },
 };
 
