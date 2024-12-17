1df1daa7af7248e8a9e648c36b544ebc461c41f8
Ben Fiola <benfiola@gmail.com>
2024-12-16 23:42:24 -0800

    add default alloy configuration

--- apps/alloy.ts
+++ apps/alloy.ts
@@ -1,6 +1,7 @@
 import { Chart, Helm } from "cdk8s";
 import { Namespace } from "../resources/k8s/k8s";
 import { CliContext, ManifestsCallback } from "../utils/CliContext";
+import { codeblock } from "../utils/codeblock";
 import { createNetworkPolicy } from "../utils/createNetworkPolicy";
 
 const appData = {
@@ -27,7 +28,78 @@ const manifests: ManifestsCallback = async (app) => {
     namespace: chart.namespace,
     values: {
       alloy: {
-        // disallow reporting
+        configMap: {
+          content: codeblock`
+            discovery.kubernetes "pod" {
+              role = "pod"
+            }
+            
+            discovery.relabel "pod_logs" {
+              targets = discovery.kubernetes.pod.targets
+
+              rule {
+                source_labels = ["__meta_kubernetes_namespace"]
+                action = "replace"
+                target_label = "namespace"
+              }
+
+              rule {
+                source_labels = ["__meta_kubernetes_pod_name"]
+                action = "replace"
+                target_label = "pod"
+              }
+
+              rule {
+                source_labels = ["__meta_kubernetes_pod_container_name"]
+                action = "replace"
+                target_label = "container"
+              }
+
+              rule {
+                source_labels = ["__meta_kubernetes_pod_label_app_kubernetes_io_name"]
+                action = "replace"
+                target_label = "app"
+              }
+
+              rule {
+                source_labels = ["__meta_kubernetes_namespace", "__meta_kubernetes_pod_container_name"]
+                action = "replace"
+                target_label = "job"
+                separator = "/"
+                replacement = "$1"
+              }
+
+              rule {
+                source_labels = ["__meta_kubernetes_pod_uid", "__meta_kubernetes_pod_container_name"]
+                action = "replace"
+                target_label = "__path__"
+                separator = "/"
+                replacement = "/var/log/pods/*$1/*.log"
+              }
+
+              rule {
+                source_labels = ["__meta_kubernetes_pod_container_id"]
+                action = "replace"
+                target_label = "container_runtime"
+                regex = "^(\\S+):\\/\\/.+$"
+                replacement = "$1"
+              }
+            }
+
+            loki.source.kubernetes "pod_logs" {
+              targets    = discovery.relabel.pod_logs.output
+              forward_to = [loki.process.pod_logs.receiver]
+            }
+
+            loki.process "pod_logs" {
+              forward_to = [loki.write.default.receiver]
+            }
+
+            loki.write "default" {
+              endpoint = "https://loki-gateway.loki.svc/loki/api/v1/push"
+            }`,
+        },
+        // disable reporting
         enableReporting: false,
       },
       crds: {
