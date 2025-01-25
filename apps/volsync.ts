764d5b27cec8f12141147c4a40b90037353166d3
Ben Fiola <benfiola@gmail.com>
2025-01-24 21:44:54 -0800

    add restore-to-pvc volsync helper method

--- apps/volsync.ts
+++ apps/volsync.ts
@@ -1,6 +1,8 @@
-import { Chart, Helm } from "cdk8s";
+import { App, Chart, Helm } from "cdk8s";
 import { writeFile } from "fs/promises";
-import { Namespace } from "../resources/k8s/k8s";
+import * as crypto from "node:crypto";
+import path from "node:path";
+import { Namespace, PersistentVolumeClaim } from "../resources/k8s/k8s";
 import {
   CliContext,
   ManifestsCallback,
@@ -9,6 +11,7 @@ import {
 import { createNetworkPolicy } from "../utils/createNetworkPolicy";
 import { exec } from "../utils/exec";
 import { getHelmTemplateCommand } from "../utils/getHelmTemplateCommand";
+import { temporaryDirectory } from "../utils/temporaryDirectory";
 
 const appData = {
   chart: "volsync",
@@ -52,7 +55,103 @@ const resources: ResourcesCallback = async (manifestFile) => {
   await writeFile(manifestFile, manifest);
 };
 
+/**
+ * Restores a volsync backup to a new pvc and prints the pvc name.
+ * NOTE: Also creates a ReplicationDestination with the same name as the created pvc.
+ * @param namespace the namespace of the pvc to restore from
+ * @param sourcePvcName the name of the pvc to restore from
+ * @param before only select snapshots before this date
+ */
+const restoreToPvc = async (
+  namespace: string,
+  sourcePvcName: string,
+  { before }: { before: Date }
+) => {
+  // NOTE: cdk8s is unable to serialize a `Date` object without this patch
+  (before as any).toJSON = () => before.toISOString();
+
+  const { ReplicationDestination } = await import(
+    "../resources/volsync/volsync.backube"
+  );
+
+  const srcPvc = JSON.parse(
+    await exec([
+      "kubectl",
+      "get",
+      "persistentvolumeclaims",
+      "--output=json",
+      `--namespace=${namespace}`,
+      sourcePvcName,
+    ])
+  );
+  const repSrc = JSON.parse(
+    await exec([
+      "kubectl",
+      "get",
+      "replicationsources",
+      "--output=json",
+      `--namespace=${namespace}`,
+      sourcePvcName,
+    ])
+  );
+
+  const id = crypto.randomBytes(3).toString("hex");
+  const app = new App();
+  const chart = new Chart(app, "chart", { namespace: namespace });
+  const pvc = new PersistentVolumeClaim(chart, "pvc", {
+    metadata: { name: `${sourcePvcName}-restore-${id}` },
+    spec: {
+      accessModes: srcPvc.spec.accessModes,
+      resources: {
+        requests: {
+          storage: { value: srcPvc.spec.resources.requests.storage },
+        },
+      },
+      storageClassName: srcPvc.spec.storageClassName,
+    },
+  });
+  new ReplicationDestination(chart, "rep-dest", {
+    metadata: { name: pvc.name },
+    spec: {
+      restic: {
+        copyMethod: "Direct" as any,
+        destinationPvc: pvc.name,
+        moverPodLabels: repSrc.spec.restic.moverPodLabels,
+        moverSecurityContext: repSrc.spec.restic.moverSecurityContext,
+        repository: repSrc.spec.restic.repository,
+        restoreAsOf: {
+          toJSON: () => "",
+          toISOString: () => before.toISOString(),
+        } as any,
+        storageClassName: repSrc.spec.restic.storageClassName,
+      },
+      trigger: {
+        manual: "manual",
+      },
+    },
+  });
+
+  await temporaryDirectory(async (directory) => {
+    const manifest = path.join(directory, "manifest.yaml");
+    await writeFile(manifest, app.synthYaml());
+    await exec(["kubectl", "apply", "-f", manifest]);
+    console.log(`created pvc: ${chart.namespace}/${pvc.name}`);
+  });
+};
+
 export default async function (context: CliContext) {
   context.manifests(manifests);
   context.resources(resources);
+  context.command((program) => {
+    program
+      .command("restore-to-pvc <namespace> <pvc-name>")
+      .option(
+        "--before <before>",
+        "consider snapshots before this date",
+        (v: string) => new Date(v),
+        new Date()
+      )
+      .description("restores a backup to a pvc")
+      .action(restoreToPvc);
+  });
 }
