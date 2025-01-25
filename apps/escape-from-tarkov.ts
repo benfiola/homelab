f57e91cf409be291db6257993de4b0b5d31f93b4
Ben Fiola <benfiola@gmail.com>
2025-01-24 16:22:08 -0800

    open acl to lfs.sp-tarkov.com for eft workload

--- apps/escape-from-tarkov.ts
+++ apps/escape-from-tarkov.ts
@@ -52,6 +52,13 @@ const manifests: ManifestsCallback = async (app) => {
     {
       from: { pod: "escape-from-tarkov" },
       to: {
+        dns: "lfs.sp-tarkov.com",
+        ports: [[443, "tcp"]],
+      },
+    },
+    {
+      from: { pod: "escape-from-tarkov" },
+      to: {
         dns: "raw.githubusercontent.com",
         ports: [[443, "tcp"]],
       },
