e6079aabeb42f3b63c8f90bfc782b3ee9f82df73
Ben Fiola <benfiola@gmail.com>
2025-01-24 18:53:32 -0800

    update eft image

--- apps/escape-from-tarkov.ts
+++ apps/escape-from-tarkov.ts
@@ -173,7 +173,7 @@ const manifests: ManifestsCallback = async (app) => {
     containers: [
       {
         envFrom: [serverSecret],
-        image: "benfiola/single-player-tarkov:0.6.0-rc.6",
+        image: "benfiola/single-player-tarkov:0.6.0-rc.7",
         mounts: {
           cache: "/cache",
           data: "/data",
