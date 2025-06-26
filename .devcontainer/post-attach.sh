6ec596038aaa005c0b9238764b4484a20e65a6c1
Ben Fiola <benfiola@gmail.com>
2025-06-25 23:21:26 -0700

    fix devcontainer

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,5 +3,5 @@ set -e
 
 gcloud auth application-default login --launch-browser
 homelab env download
-homelab nodes config download
-talosctl -n node-a.cluster.bulia kubeconfig --force
+homelab talos download
+homelab talos kubeconfig
