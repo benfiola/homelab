07bd73e6c99bb08e14e73b8a78c47de7bd0a578c
Ben Fiola <benfiola@gmail.com>
2025-07-27 11:08:01 -0700

    fix devcontainer create script

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,5 +3,6 @@ set -e
 
 gcloud auth application-default login --launch-browser
 homelab env download
+homelab talos download
 homelab talos generate-talosconfig
 homelab talos generate-kubeconfig a
