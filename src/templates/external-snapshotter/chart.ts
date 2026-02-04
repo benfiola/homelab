d22ea708a18e301ce12d5d683b4107780cbe35ff
Ben Fiola <benfiola@gmail.com>
2026-02-03 18:50:59 -0800

    set security context for external-snapshotter

--- src/templates/external-snapshotter/chart.ts
+++ src/templates/external-snapshotter/chart.ts
@@ -1,11 +1,13 @@
 import {
   Chart,
   findApiObject,
+  getSecurityContext,
   Kustomization,
   Namespace,
   VerticalPodAutoscaler,
 } from "../../cdk8s";
 import { TemplateChartFn } from "../../context";
+import { stringify } from "../../yaml";
 
 export const chart: TemplateChartFn = async (construct, _, context) => {
   const chart = new Chart(construct, context.name);
@@ -13,10 +15,34 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
 
   new Namespace(chart);
 
+  const securityContext = getSecurityContext();
+
   await Kustomization.init(chart, `${id}-kustomization`, {
     dynamic: {
       namespace: chart.namespace,
       resources: [context.getAsset("manifest.yaml")],
+      patchesJson6902: [
+        {
+          target: {
+            group: "apps",
+            version: "v1",
+            kind: "Deployment",
+            name: "snapshot-controller",
+          },
+          patch: stringify([
+            {
+              op: "replace",
+              path: "/spec/template/spec/securityContext",
+              value: securityContext.pod,
+            },
+            {
+              op: "replace",
+              path: "/spec/template/spec/containers/0/securityContext",
+              value: securityContext.container,
+            },
+          ]),
+        },
+      ],
     },
   });
 
@@ -26,7 +52,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
       apiVersion: "apps/v1",
       kind: "Deployment",
       name: "snapshot-controller",
-    })
+    }),
   );
 
   return chart;
