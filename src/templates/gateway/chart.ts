f1e84b1f7a241f2f6b0356e9c211385c684a5407
Ben Fiola <benfiola@gmail.com>
2026-02-14 16:39:35 -0800

    set annotations correctly for gateway resources

--- src/templates/gateway/chart.ts
+++ src/templates/gateway/chart.ts
@@ -19,16 +19,16 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
   });
 
   for (const name of gateways) {
-    const annotations: Record<string, string> = {};
+    const annotations: Record<string, string> = {
+      "cert-manager.io/cluster-issuer": "cloudflare",
+    };
     if (name === "public") {
       annotations["external-dns.alpha.kubernetes.io/target"] =
         "current.bfiola.dev";
     }
     new WrappedGateway(chart, `${id}-wrapped-gateway-${name}`, {
       metadata: {
-        annotations: {
-          "cert-manager.io/cluster-issuer": "cloudflare",
-        },
+        annotations,
         name,
       },
       spec: {
