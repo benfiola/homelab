846e5442d06d91538e3114c746fdd29f968a3038
Ben Fiola <benfiola@gmail.com>
2026-02-28 23:41:07 -0800

    add garage key/bucket for minecraft

--- src/templates/minecraft/chart.ts
+++ src/templates/minecraft/chart.ts
@@ -1,11 +1,28 @@
 import { Service, StatefulSet } from "../../../assets/kubernetes/k8s";
-import { Chart, getSecurityContext, Namespace, TcpRoute } from "../../cdk8s";
+import {
+  Chart,
+  GarageBucket,
+  GarageKey,
+  getSecurityContext,
+  Namespace,
+  TcpRoute,
+  VaultAuth,
+} from "../../cdk8s";
 import { TemplateChartFn } from "../../context";
 
 export const chart: TemplateChartFn = async (construct, id) => {
   const chart = new Chart(construct, id, { namespace: id });
 
   new Namespace(chart);
+
+  const auth = new VaultAuth(chart);
+
+  const key = new GarageKey(chart, "garage", id, auth, {
+    accessKeyId: "s3-access-key-id",
+    secretAccessKey: "s3-secret-access-key",
+  });
+
+  const bucket = new GarageBucket(chart, "garage", id, [key]);
 
   const securityContext = getSecurityContext({ uid: 1000, gid: 1000 });
 
