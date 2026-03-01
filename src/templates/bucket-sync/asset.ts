3f3a3c9292d5c2ebd82e1cf8976cbe70026061a3
Ben Fiola <benfiola@gmail.com>
2026-02-28 22:09:02 -0800

    Add bucket sync resources

--- src/templates/bucket-sync/asset.ts
+++ src/templates/bucket-sync/asset.ts
@@ -1,8 +1,10 @@
-import { rename } from "fs/promises";
+import { rename, writeFile } from "fs/promises";
 import { join } from "path";
+import * as cdk8s from "../../cdk8s-cli.ts";
 import { TemplateAssetFn } from "../../context.ts";
 import * as helm from "../../helm.ts";
 import { homelabHelper } from "../../homelab-helper.ts";
+import { getTempy } from "../../tempy.ts";
 
 export const assets: TemplateAssetFn = async (dir) => {
   const sourceChartPath = await helm.pull(
@@ -11,4 +13,16 @@ export const assets: TemplateAssetFn = async (dir) => {
   );
   const destChartPath = join(dir, "chart.tar.gz");
   await rename(sourceChartPath, destChartPath);
+
+  const manifest = await helm.template({
+    chart: destChartPath,
+  });
+
+  const tempy = await getTempy();
+  await tempy.temporaryDirectoryTask(async (tempDir: string) => {
+    const manifestPath = join(tempDir, "manifest.yaml");
+    await writeFile(manifestPath, manifest);
+
+    await cdk8s.importResources(manifestPath, dir);
+  });
 };
