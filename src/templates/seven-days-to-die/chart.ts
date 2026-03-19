81fb109aa8740472bf9b3887104f0d6064987696
Ben Fiola <benfiola@gmail.com>
2026-03-19 16:58:25 -0700

    bump game server images

--- src/templates/seven-days-to-die/chart.ts
+++ src/templates/seven-days-to-die/chart.ts
@@ -39,7 +39,7 @@ export const chart: TemplateChartFn = async (construct, id) => {
             {
               name: "seven-days-to-die",
               image:
-                "ghcr.io/benfiola/game-server-images/seven-days-to-die:0.1.0-alpha-feat-initial.29",
+                "ghcr.io/benfiola/game-server-images/seven-days-to-die:0.1.0-alpha-feat-initial.30",
               ports: [
                 {
                   name: "tcp",
