78680d6f707b90e7f489f85d272a7fd2b1900e15
Ben Fiola <benfiola@gmail.com>
2026-03-22 21:10:35 -0700

    add config patches and raid-review ports to spt

--- src/templates/single-player-tarkov/chart.ts
+++ src/templates/single-player-tarkov/chart.ts
@@ -3,6 +3,7 @@ import {
   Chart,
   getAssetsServerUrl,
   getSecurityContext,
+  HttpRoute,
   Namespace,
   TcpRoute,
 } from "../../cdk8s";
@@ -25,6 +26,27 @@ export const chart: TemplateChartFn = async (construct, id) => {
     "StatTrack-2.0.0.7z",
     "UIFixes-5.3.7.zip",
   ].map((m) => getAssetsServerUrl(`single-player-tarkov/${m}`));
+
+  const configPatches = {
+    // immediate return of insurance items
+    "SPT_Data/configs/insurance.json": [
+      { op: "replace", path: "/runIntervalSeconds", value: 60 },
+    ],
+    "SPT_Data/database/traders/54cb57776803fa99248b456e/base.json": [
+      { op: "replace", path: "/insurance/min_return_hour", value: 0 },
+      { op: "replace", path: "/insurance/max_return_hour", value: 0 },
+    ],
+    "SPT_Data/database/traders/54cb50c76803fa8b248b4571/base.json": [
+      { op: "replace", path: "/insurance/min_return_hour", value: 0 },
+      { op: "replace", path: "/insurance/max_return_hour", value: 0 },
+    ],
+
+    // immediate scav run cooldown
+    "SPT_Data/database/globals.json": [
+      { op: "replace", path: "/config/SavagePlayCooldown", value: 1 },
+      { op: "replace", path: "/config/SavagePlayCooldownNdaFree", value: 1 },
+    ],
+  };
 
   new StatefulSet(chart, `${id}-stateful-set`, {
     metadata: {
@@ -49,19 +71,31 @@ export const chart: TemplateChartFn = async (construct, id) => {
               image: gameServerImage("single-player-tarkov"),
               ports: [
                 {
-                  name: "tcp",
+                  name: "game",
                   containerPort: 6969,
+                },
+                {
+                  name: "raid-rev-sock",
+                  containerPort: 7828,
+                },
+                {
+                  name: "raid-rev-web",
+                  containerPort: 7829,
                 },
               ],
               securityContext: securityContext.container,
               volumeMounts: [{ name: "cache", mountPath: "/cache" }],
               env: [
-                { name: "VERSION", value: "4.0.13" },
-                { name: "LOG_LEVEL", value: "debug" },
+                {
+                  name: "CONFIG_PATCHES",
+                  value: JSON.stringify(configPatches),
+                },
                 {
                   name: "MOD_URLS",
                   value: mods.join(","),
                 },
+                { name: "LOG_LEVEL", value: "debug" },
+                { name: "VERSION", value: "4.0.13" },
               ],
             },
           ],
@@ -98,13 +132,26 @@ export const chart: TemplateChartFn = async (construct, id) => {
       ports: [
         {
           port: 6969,
-          name: "tcp",
+          name: "game",
+        },
+        {
+          port: 7828,
+          name: "raid-rev-sock",
+        },
+        {
+          port: 7829,
+          name: "raid-rev-web",
         },
       ],
     },
   });
 
   new TcpRoute(chart, "trusted", "spt.bulia.dev", 6969, service, 6969);
+  new TcpRoute(chart, "trusted", "spt.bulia.dev", 7828, service, 7828);
+  new HttpRoute(chart, "trusted", "raid-review.spt.bulia.dev").match(
+    service,
+    7829,
+  );
 
   return chart;
 };
