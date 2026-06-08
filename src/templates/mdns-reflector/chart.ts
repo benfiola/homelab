f98a02f010315ef230a1489e0fe14f09c817c109
Ben Fiola <benfiola@gmail.com>
2026-06-08 13:25:19 -0700

    re-enable mdns-reflector, but remove 'init' daemonset in favor of talos provisioned bridge

--- src/templates/mdns-reflector/chart.ts
+++ src/templates/mdns-reflector/chart.ts
@@ -1,15 +1,11 @@
-import dedent from "ts-dedent";
-import { ConfigMap } from "../../../assets/kubernetes/k8s";
 import {
   Chart,
-  DaemonSet,
   findApiObject,
   Helm,
   Namespace,
   VerticalPodAutoscaler,
 } from "../../cdk8s";
 import { TemplateChartFn } from "../../context";
-import { alpineImage } from "../../image-refs";
 
 export const chart: TemplateChartFn = async (construct, _, context) => {
   const id = context.name;
@@ -17,38 +13,38 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
 
   new Namespace(chart, { privileged: true });
 
-  const scripts = new ConfigMap(chart, `${id}-config-map-scripts`, {
-    metadata: {
-      name: "mdns-reflector-scripts",
-    },
-    data: {
-      "init.sh": dedent`
-        #!/bin/sh
-        set -e
-        echo "sleeping for 5 seconds"
-        sleep 5
-        echo "checking for mdns0"
-        ip link show mdns0
-        echo "mdns0 found - waiting for termination"
-        sleep infinity
-      `,
-    },
-  });
+  // const scripts = new ConfigMap(chart, `${id}-config-map-scripts`, {
+  //   metadata: {
+  //     name: "mdns-reflector-scripts",
+  //   },
+  //   data: {
+  //     "init.sh": dedent`
+  //       #!/bin/sh
+  //       set -e
+  //       echo "sleeping for 5 seconds"
+  //       sleep 5
+  //       echo "checking for mdns0"
+  //       ip link show mdns0
+  //       echo "mdns0 found - waiting for termination"
+  //       sleep infinity
+  //     `,
+  //   },
+  // });
 
-  const daemonSetInit = new DaemonSet(chart, "mdns-interface-init", {
-    volumes: {
-      scripts: { configMap: scripts.name },
-    },
-    podAnnotations: {
-      "k8s.v1.cni.cncf.io/networks": "multus-network/mdns@mdns0",
-    },
-  });
-  daemonSetInit.addContainer("mdns-interface-init", alpineImage, {
-    args: ["sh", "/scripts/init.sh"],
-    volumeMounts: {
-      scripts: "/scripts",
-    },
-  });
+  // const daemonSetInit = new DaemonSet(chart, "mdns-interface-init", {
+  //   volumes: {
+  //     scripts: { configMap: scripts.name },
+  //   },
+  //   podAnnotations: {
+  //     "k8s.v1.cni.cncf.io/networks": "multus-network/mdns@mdns0",
+  //   },
+  // });
+  // daemonSetInit.addContainer("mdns-interface-init", alpineImage, {
+  //   args: ["sh", "/scripts/init.sh"],
+  //   volumeMounts: {
+  //     scripts: "/scripts",
+  //   },
+  // });
 
   new Helm(chart, `${id}-helm`, context.getAsset("chart.tar.gz"), {
     config: {
@@ -59,7 +55,6 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
     },
   });
 
-  new VerticalPodAutoscaler(chart, daemonSetInit);
   new VerticalPodAutoscaler(
     chart,
     findApiObject(chart, {
