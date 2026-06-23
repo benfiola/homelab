4795966a52588738d50c52c5b84abdbf58e76aea
Ben Fiola <benfiola@gmail.com>
2026-06-23 12:14:19 -0700

    fix demo streams

--- src/templates/frigate/chart.ts
+++ src/templates/frigate/chart.ts
@@ -199,7 +199,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
         demoVideos.flatMap((url, i) => [
           [
             `MTX_PATHS_DEMO${i}_RUNONINIT`,
-            `ffmpeg -re -stream_loop -1 -i ${url} -c:v copy -an -f rtsp rtsp://localhost:8553/demo${i}`,
+            `ffmpeg -re -stream_loop -1 -i ${url} -c:v copy -bsf:v h264_mp4toannexb -an -f rtsp rtsp://localhost:8553/demo${i}`,
           ],
           [`MTX_PATHS_DEMO${i}_RUNONINITRESTART`, "yes"],
         ]),
