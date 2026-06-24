8656bcb4532a26e1d09b4a5df29b2f06420ff5db
Ben Fiola <benfiola@gmail.com>
2026-06-23 20:19:48 -0700

    enable debug logging for home-assistant

--- src/templates/home-assistant/chart.ts
+++ src/templates/home-assistant/chart.ts
@@ -26,6 +26,9 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
           use_x_forwarded_for: true,
           trusted_proxies: ["10.244.0.0/16"],
         },
+        logger: {
+          defaut: "debug",
+        },
         mobile_app: {},
         zeroconf: {},
       }),
