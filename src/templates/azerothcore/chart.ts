d4d094c7465591f5eff5b097b6dfdcfc6d3bbd45
Ben Fiola <benfiola@gmail.com>
2026-06-30 21:06:31 -0700

    update the azerothcore image

--- src/templates/azerothcore/chart.ts
+++ src/templates/azerothcore/chart.ts
@@ -17,7 +17,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
 
   new Namespace(chart);
 
-  const image = "ghcr.io/benfiola/homelab-images/azerothcore:1.1.2";
+  const image = "ghcr.io/benfiola/homelab-images/azerothcore:1.1.5";
   const hostname = "wow.bulia.dev";
   const dbHost = "db.azerothcore.svc";
   const dbPort = 3306;
