9567f07fc9198d1f6bd30968d127eb1d696597dd
Ben Fiola <benfiola@gmail.com>
2026-07-03 12:19:08 -0700

    increase jellyfin storage space

--- src/templates/mediarr/chart.ts
+++ src/templates/mediarr/chart.ts
@@ -110,7 +110,7 @@ export const chart: TemplateChartFn = async (construct, id) => {
   const jellyfin = new StatefulSet(chart, "jellyfin", {
     securityContext: { uid: 0, gid: 0, caps: ["CHOWN", "SETUID", "SETGID"] },
     volumes: {
-      config: { pvc: { size: "1Gi", storageClass: "standard" } },
+      config: { pvc: { size: "3Gi", storageClass: "standard" } },
       data: { pvc: { name: "data" } },
     },
   });
