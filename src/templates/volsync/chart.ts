656188f555673202aff866636e0d193cfff4a85d
Ben Fiola <benfiola@gmail.com>
2026-07-29 21:31:27 -0700

    remove test pvc, deployment, backup from volsync

--- src/templates/volsync/chart.ts
+++ src/templates/volsync/chart.ts
@@ -1,16 +1,9 @@
 import {
-  PersistentVolumeClaim,
-  Quantity,
-} from "../../../assets/kubernetes/k8s";
-import {
   Chart,
-  Deployment,
   findApiObject,
   Helm,
   Namespace,
   VerticalPodAutoscaler,
-  VolsyncAuth,
-  VolsyncBackup,
 } from "../../cdk8s";
 import { TemplateChartFn } from "../../context";
 
@@ -30,40 +23,6 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
       limits: null,
     },
   });
-
-  const pvc = new PersistentVolumeClaim(chart, `${id}-test-pvc`, {
-    metadata: {
-      name: "testing",
-    },
-    spec: {
-      accessModes: ["ReadWriteOnce"],
-      storageClassName: "standard",
-      resources: {
-        requests: {
-          storage: Quantity.fromString("2Gi"),
-        },
-      },
-    },
-  });
-  const deployment = new Deployment(chart, "testing", {
-    volumes: {
-      test: { pvc: { name: pvc.name } },
-    },
-  });
-  deployment.addContainer(
-    "testing",
-    "ghcr.io/benfiola/homelab-images/toolbox:1.1.0",
-    {
-      cmd: ["bash", "-c"],
-      args: ["sleep infinity"],
-      volumeMounts: {
-        test: "/data",
-      },
-    },
-  );
-
-  const vsAuth = new VolsyncAuth(chart);
-  new VolsyncBackup(chart, vsAuth, pvc.name);
 
   new VerticalPodAutoscaler(
     chart,
