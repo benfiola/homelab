6356f9feed007873c4c6c44ef9442d5ab511adf0
Ben Fiola <benfiola@gmail.com>
2026-08-16 16:05:51 -0700

    add music-assistant ingress tab to home-assistant

--- src/templates/network-policy/chart.ts
+++ src/templates/network-policy/chart.ts
@@ -168,6 +168,10 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
     _assetsServer("minecraft"),
   );
   const mosquitto = svc("mosquitto", pod("mosquitto", "mosquitto"));
+  const musicAssistant = svc(
+    "music-assistant",
+    pod("music-assistant", "music-assistant"),
+  );
   const nodeFeatureDiscovery = svc(
     "node-feature-discovery",
     pod("node-feature-discovery", "node-feature-discovery"),
@@ -419,6 +423,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
     .to(frigate, tcp(5000, 8554, 8555), udp(8555))
     .to(llamaCppServer, tcp(8080))
     .to(mosquitto, tcp(1883))
+    .to(musicAssistant, tcp(8095))
     .to(wyomingPiper, tcp(10200))
     .to(wyomingWhisper, tcp(10300))
     .to(dns("mobile-apps.home-assistant.io"), tcp(443));
