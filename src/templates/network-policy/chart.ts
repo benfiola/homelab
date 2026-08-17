a1ebaa1cc11a3dc9d5a0ecdbd3a587d6ebf43d20
Ben Fiola <benfiola@gmail.com>
2026-08-16 22:39:17 -0700

    remove extra music-assistant acls, remove music-assistant app - just to clean things up

--- src/templates/network-policy/chart.ts
+++ src/templates/network-policy/chart.ts
@@ -541,9 +541,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
 
   // music-assistant
   musicAssistant
-    .to(cidrs("224.0.0.251/32"), udp(5353))
     .to(cidrs("192.168.24.0/24"))
-    .to(gatewayFamily, tcp(8097))
     .to(dns("tidal.com"), tcp(443))
     .to(dns("*.tidal.com"), tcp(443));
 
