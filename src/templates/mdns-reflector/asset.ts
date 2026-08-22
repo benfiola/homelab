6ed2fb37287e61d413252f124211de4eb058a7e6
Ben Fiola <benfiola@gmail.com>
2026-08-21 21:11:28 -0700

    bump mdns-reflector to version 2.0.3

--- src/templates/mdns-reflector/asset.ts
+++ src/templates/mdns-reflector/asset.ts
@@ -7,7 +7,7 @@ export const assets: TemplateAssetFn = async (dir) => {
   const sourceChartPath = await helm.pull(
     {
       chart: "oci://ghcr.io/benfiola/homelab-images/charts/mdns-reflector",
-      version: "2.0.2",
+      version: "2.0.3",
     },
     dir,
   );
