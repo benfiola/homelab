442b0662e0c9bb5a2d309fe9052035015b09820f
Ben Fiola <benfiola@gmail.com>
2026-08-24 14:22:29 -0700

    remove unused download script from wyoming-whisper, use larger stt model

--- src/templates/wyoming-whisper/chart.ts
+++ src/templates/wyoming-whisper/chart.ts
@@ -30,7 +30,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
       args: [
         "--hass-token=$(HASS_TOKEN)",
         "--hass-api=http://home-assistant.home-assistant.svc:8123/api",
-        "--model=rhasspy/faster-whisper-base-int8",
+        "--model=large-v3-turbo",
         "--language=en",
         "--uri=tcp://0.0.0.0:10300",
         "--data-dir=/data",
