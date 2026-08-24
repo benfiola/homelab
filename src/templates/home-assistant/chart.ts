b7eb8da8d988d385fa18b5c3abcfdb125ca1207d
Ben Fiola <benfiola@gmail.com>
2026-08-24 15:57:03 -0700

    update home-assistant image to also include llm-vision blueprint

--- src/templates/home-assistant/chart.ts
+++ src/templates/home-assistant/chart.ts
@@ -66,7 +66,7 @@ export const chart: TemplateChartFn = async (construct, _, context) => {
   });
   statefulSet.addContainer(
     "home-assistant",
-    "ghcr.io/benfiola/homelab-images/home-assistant:1.7.0",
+    "ghcr.io/benfiola/homelab-images/home-assistant:1.7.1",
     {
       env: {
         TZ: "America/Los_Angeles",
